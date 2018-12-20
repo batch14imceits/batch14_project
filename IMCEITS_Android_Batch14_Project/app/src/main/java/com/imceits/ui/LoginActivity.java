9b0e67eea7f6829f0f5010e4bc458d82b700089f
batch14imceits <batch14imceits@gmail.com>
2018-12-20 11:04:22 +0630

    Test from school PC

--- IMCEITS_Android_Batch14_Project/app/src/main/java/com/imceits/ui/LoginActivity.java
+++ IMCEITS_Android_Batch14_Project/app/src/main/java/com/imceits/ui/LoginActivity.java
@@ -2,6 +2,7 @@ package com.imceits.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frm_login);
+
+        
+
     }
 }
